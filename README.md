8df7713d90617f79429ddd9b74a944dbe9b9d614
Dinson-mac <dinson@slashbeyond.com>
2020-05-07 15:39:32 +0530

    updated output

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 ## Tor Status Checker
+Check status of Dark web and open web sites.
+
+Need TOR client installed in system.
+
+For mac run :-
+
+<code>brew install tor</code>
+
+For linux :-
+
+<code>sudo apt install tor</code>
+
+Once Tor is up and running, run the 'main.py' python code in terminal, passing the URL to check as CLI parameter
+
+Eg:- <code>python3 main.py https://examplesite.onion</code>
+
+The code will output the response status code to the terminal.
+
---
+++ main.py
@@ -0,0 +1,40 @@
+# Check if a TOR site is up and running 
+# Can be used to check status of both open web and dark web sites
+# @author Dinson David Kurian <dinson@technisanct.com>
+# May 6th, 2020
+
+import requests
+import sys
+
+# get url from CLI parameter
+url = sys.argv[1]
+
+session = requests.session()
+session.proxies = {}
+
+session.proxies['http'] = 'socks5h://127.0.0.1:9050'
+session.proxies['https'] = 'socks5h://127.0.0.1:9050'
+
+print("\nConnected to TOR...")
+
+# they would know we are a Python script, what library we are using, and some version info. 
+# We can change this.
+# We will create some request headers and change the User-agent:
+headers = {}
+headers['User-agent'] = 'HotJava/1.1.2 FCS'
+# Then we include the headers in the request:
+
+try:
+    
+    r = session.get(url, headers=headers)
+    print("\nConnection SUCCESS! : " + url)
+    print("\nStatus Code: " + str(r.status_code))
+    # uncomment following for loop to view all headers
+    # for key in r.headers:
+    #     print(key + ": " + r.headers[key])
+
+except:
+    
+    print("Connection FAILED!")
+
+print("\nDone!\n")
